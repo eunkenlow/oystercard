238931f01f96a2a2a08d6bdd7431f7be2b875934
elizabethvenner <elizabethvenner@googlemail.com>
2016-10-03 15:19:03 +0100

    4. Write and test code to top up card

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,4 +7,10 @@ describe Oystercard do
     expect(card.balance).to eq 0
   end
 
+  it {is_expected.to respond_to(:top_up).with(1).argument}
+  it "tops up the oyster card" do
+    card.top_up(20)
+    expect(card.balance).to eq 20
+  end
+
 end
