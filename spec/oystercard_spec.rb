229e9ba42b496d5b332c6f4e38d6ddf7a57104dc
eunkenlow <eunkenlow@gmail.com>
2016-10-03 15:04:33 +0100

    challenge 3: added oystercard.rb

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,10 @@
-describe Oystercard
+require 'oystercard'
+describe Oystercard do
+
+  subject(:card) {described_class.new}
+
+  it "has a balance" do
+    expect(card.balance).to eq 0
+  end
+
 end
