924524c8a0cd5bad25a9da4ce6f99e0f63ad595f
elizabethvenner <elizabethvenner@googlemail.com>
2016-10-03 15:54:42 +0100

    4. Write and test code to top up cardexit

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,7 +7,6 @@ describe Oystercard do
     expect(card.balance).to eq 0
   end
 
-  it {is_expected.to respond_to(:top_up).with(1).argument}
   it "tops up the oyster card" do
     expect{ card.top_up 20 }.to change{ card.balance }.by 20
   end
@@ -17,4 +16,6 @@ describe Oystercard do
     card.top_up maximum
     expect{card.top_up(1)}.to raise_error "Balance exceeded maximum limit of #{Oystercard::MAXIMUM_LIMIT}"
   end
+
+  it {is_expected.to respond_to(:deduct).with(1).argument}
 end
